23b004278c30f2544f34bb7411b7525486c42a80
pedro <pedro.paz13@hotmail.com>
2023-04-17 15:41:05 -0300

    Adicionando atualizacoes na classe float

--- src/TipoFloat.java
+++ src/TipoFloat.java
@@ -1,4 +1,15 @@
 
 public class TipoFloat {
+	
+	/**
+	 *É o tipo de dado capaz de armazenar números reais de precisão simples, ou seja, 32 bits de informação representando um número real.
+Exemplos de declarações: 
+	 */
+	
+	float a;
+	float by1 = -32.0f;
+	float bz2 = 32.2F;
+	float bz = 32.455f;
+	float bze = 1.32455e4f;
 
 }
