09edb5c146487009530f1b625d296e88aed87f45
pedro <pedro.paz13@hotmail.com>
2023-04-17 15:42:51 -0300

    adicionando mais uma classe ao nosso projeto

---
+++ src/TipoDouble.java
@@ -0,0 +1,14 @@
+
+public class TipoDouble {
+	/**
+	 * É o tipo de dado capaz de armazenar números reais de precisão dupla, ou seja, 64 bits de informação em forma de número real. É usado para representar valores nos quais é preciso uma precisão maior que a de float.
+	 *	Exemplos de declarações
+	 */
+	
+	double a;
+	double by1 = -32.0;
+	double bz2 = 32.2d;
+	double bz = 32.455D;
+	double bze = 1.32455e4D;
+
+}
