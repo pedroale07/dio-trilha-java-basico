0c8fc512529fb00ce29ea05bc06af4e558f59f24
pedro <pedro.paz13@hotmail.com>
2023-04-17 14:24:00 -0300

    adicionando mais uma classe TipoInt aos dados do projeto

---
+++ src/TipoInt.java
@@ -0,0 +1,13 @@
+
+public class TipoInt {
+	
+	/**
+	 * O tipo int é um tipo primitivo do Java
+	 * possui 4 bytes de formacao e 32bits
+	 * 
+	 * O tipo int são os famosos tipo inteiro, não possuindo casas decimas com virgulas
+	 */
+	
+	int VariavelInt = 3;
+
+}
