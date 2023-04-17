e8c4c42bed49883d9b96f952b787899bc662bf70
pedro <pedro.paz13@hotmail.com>
2023-04-17 13:40:48 -0300

    =Iniciando comentario de documentacao e descrevendo classes

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -3,7 +3,37 @@ public class TiposVariaveis {
 
 	public static void main(String[] args) {
 		
+	//autor: pedro alessandro
+		
+		
+		/**
+		 * iremos trabalhar dentro desse modulo as variaveis e suas funções
+		 * 
+		 */
 
+		/**
+		 * No java desde o inicio existem palavras que caracterizam e representam os tipos
+		 * de dados basicos, que precisam ser usados e atribuidos aos dados para criar programas e sistemas
+		 * 
+		 */
+		
+			/**
+			 * temos como principio primitivo os seguintes tipos de variaveis; int, byte, short, long, float, double, boolean
+			 * e char
+			 * 
+			 * !esses tipos nao sao considerados objetos e portanto representam valores brutos!
+			 *  
+			 */
+		
+		//TIPOS DE DADOS
+		
+		//BYTE
+		/**
+		 * Adentre na classe abaixo e verifique suas respectivas funcoes e seu uso
+		 */
+		
+			
+		
 	}
 
 }
