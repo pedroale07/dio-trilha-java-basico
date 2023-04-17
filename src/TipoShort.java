0152bb9ae5429641c08e887c6ef9cb42dc766b06
pedro <pedro.paz13@hotmail.com>
2023-04-17 14:24:58 -0300

    Subindo dados atualizados de informacoes da classe

--- src/TipoShort.java
+++ src/TipoShort.java
@@ -1,4 +1,14 @@
 
 public class TipoShort {
+	/**
+	 * short é um tipo de dado primitivo do java podendo ir de -32.768 a 32.767
+	 * Uma variavel do tipo short possui 2 bytes de formacao ou 16 bits 
+	 */
+	
+	 short tipoShort = 32767;
+	 
+		
+	
+	
 
 }
